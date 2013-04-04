b0b7327158c13b9f1976f6df3bf4c8aa3ceb0ed9
Jessica T <xray7224@googlemail.com>
2013-04-04 13:27:45 +0100

    Splits the domaina and the alias up

--- inboxen/inboxen/models.py
+++ inboxen/inboxen/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Domain(models.Model):
+    # these are the domains available to create aliases from
+    domain = models.CharField(max_length=256)
+
 class Alias(models.Model):
-	alias = models.CharField(max_length=1024)
-	user = models.ForeignKey(User) 
+    alias = models.CharField(max_length=512)
+    domain = models.ForeignKey(Domain)
+    user = models.ForeignKey(User) 
 
 class Attachment(models.Model):
-	pass
+    pass
 
 class Tag(models.Model):
     alias = models.ForeignKey(Alias)
     tag = models.CharField(max_length=256)
 
 class Header(models.Model):
-	name = models.CharField(max_length=1024)
-	data = models.CharField(max_length=1024)	
+    name = models.CharField(max_length=1024)
+    data = models.CharField(max_length=1024)    
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
@@ -22,9 +27,9 @@ class UserProfile(models.Model):
     premium = models.BooleanField()  
 
 class Email(models.Model):
-	headers = models.ManyToManyField(Header)
-	user = models.ForeignKey(User, related_name='user')
-	inbox = models.ForeignKey(Alias)
-	body = models.TextField()
-	attachments = models.ManyToManyField(Attachment)
-	recieved_date = models.DateTimeField('Recieved Date')
+    headers = models.ManyToManyField(Header)
+    user = models.ForeignKey(User, related_name='user')
+    inbox = models.ForeignKey(Alias)
+    body = models.TextField()
+    attachments = models.ManyToManyField(Attachment)
+    recieved_date = models.DateTimeField('Recieved Date')
