ae134881e1681678cf9b7611829cd329ba4600b2
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-04-11 01:17:44 +0100

    OK, now I understand how these states are meant to work

--- inboxen/app/handlers/in.py
+++ inboxen/app/handlers/in.py
@@ -6,9 +6,6 @@ from app.model.alias import alias_exists
 @route("(alias)@(domain)", alias=".+", domain=".+")
 def START(message, alias=None, domain=None):
     if alias_exists(alias, domain):
-        return ACCEPT_MSG
-
-@route(".+")
-def ACCEPT_MSG(message):
-    accept_queue = Queue(accepted_queue_dir, **accepted_queue_opts)
-    accept_queue.push(message)
+        accept_queue = Queue(accepted_queue_dir, **accepted_queue_opts)
+        accept_queue.push(message)
+        return START
