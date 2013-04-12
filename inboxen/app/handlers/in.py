3d888fbb95e157ebe9044a077df971cf47605a66
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-04-12 01:51:02 +0100

    Make input queue stateless and non-locking, file system level locking should be enough for us

--- inboxen/app/handlers/in.py
+++ inboxen/app/handlers/in.py
@@ -1,11 +1,13 @@
-from lamson.routing import route
+from lamson.routing import route, stateless, nolocking
 from lamson.queue import Queue
 from config.settings import accepted_queue_dir, accepted_queue_opts
 from app.model.alias import alias_exists
 
 @route("(alias)@(domain)", alias=".+", domain=".+")
+@stateless
+@nolocking
 def START(message, alias=None, domain=None):
+    """Does this alias exist? If yes, queue it. If no, drop it."""
     if alias_exists(alias, domain):
         accept_queue = Queue(accepted_queue_dir, **accepted_queue_opts)
         accept_queue.push(message)
-        return START
