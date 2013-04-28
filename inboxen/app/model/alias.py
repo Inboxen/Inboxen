474687ce07d3cd5cf40ad809620f5a75529e9489
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-04-28 13:59:22 +0100

    QuerySet.exists() is slightly quicker
    see https://docs.djangoproject.com/en/dev/ref/models/querysets/#django.db.models.query.QuerySet.exists

--- inboxen/app/model/alias.py
+++ inboxen/app/model/alias.py
@@ -1,13 +1,13 @@
 from inboxen.models import Alias, Domain
 
 def alias_exists(alias, domain):
-    if Alias.objects.filter(alias=alias, domain__domain=domain):
+    if Alias.objects.filter(alias=alias, domain__domain=domain).exists():
         return True
     else:
         return False
 
 def domain_exists(domain):
-    if Domain.objects.filter(domain=domain):
+    if Domain.objects.filter(domain=domain).exists():
         return True
     else:
         return False
