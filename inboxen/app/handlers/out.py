c11348e322768272116b3e7bf7c6b1a4fa78f164
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-04-28 13:56:38 +0100

    Make the out handler non-locking, queue will handle locking on the input, django/sql will handle locking on the output

--- inboxen/app/handlers/out.py
+++ inboxen/app/handlers/out.py
@@ -1,7 +1,8 @@
-from lamson.routing import route, stateless
+from lamson.routing import route, stateless, nolocking
 from app.model.email import make_email
 
 @route("(alias)@(domain)", alias=".+", domain=".+")
+@nolocking
 @stateless
 def START(message, alias=None, domain=None):
     make_email(message, alias, domain)
