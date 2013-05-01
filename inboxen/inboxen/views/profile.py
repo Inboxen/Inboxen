aaa878f03eeeeb8cc1f0f461bf6b9f82e3d6d433
xray7224 <xray7224@googlemail.com>
2013-05-01 13:16:53 +0100

    Fixes tags not showing

--- inboxen/inboxen/views/profile.py
+++ inboxen/inboxen/views/profile.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 
 from django.contrib.auth.models import Group
-from inboxen.models import Alias, UserProfile
+from inboxen.models import Alias, UserProfile, Tag
 from inboxen.helper.user import user_profile
 
 @login_required
