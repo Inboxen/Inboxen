bdd429430ba4006ce524b628c666a452610ad5d4
xray7224 <xray7224@googlemail.com>
2013-05-13 20:30:40 +0100

    I might help some email viewing problems when html preference is selected

--- inboxen/inboxen/helper/email.py
+++ inboxen/inboxen/helper/email.py
@@ -43,7 +43,7 @@ def get_email(user, email_id, preference=None):
     plain_attachments = email.attachments.filter(content_type="text/plain")
     html_attachments = email.attachments.filter(content_type="text/html")
     
-    if email.body and html_preference < 2:
+    if email.body and (html_preference < 2 or not html_attachments.exists()):
         # I think we can give them this?
         # I hope noone sets HTML in the email.body
         message["body"] = email.body
