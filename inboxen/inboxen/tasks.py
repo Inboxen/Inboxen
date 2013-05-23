aad79b49f3e03f8a52a2a49395c4d91296d4c741
xray7224 <xray7224@googlemail.com>
2013-05-23 04:02:21 +0100

    Isdabizda
    And we finally fixed celeryd tasks

--- inboxen/inboxen/tasks.py
+++ inboxen/inboxen/tasks.py
@@ -4,13 +4,12 @@ from inboxen.models import Tag, Alias, Domain, Email
 from celery import task
 
 @task(default_retry_delay=5 * 60) # 5 minutes
-@transaction.commit_on_success
 def delete_alias(email, user):
     alias, domain = email.split("@", 1)
     
     try:
         domain = Domain.objects.get(domain=domain)
-        alias = Alias.objects.get(alias=alias, domain=domain, user=user, deleted=False)
+        alias = Alias.objects.get(alias=alias, domain=domain, user=user)
     except Alias.DoesNotExist:
         return False
 
@@ -20,20 +19,20 @@ def delete_alias(email, user):
     # it seems to cause problems if you do QuerySet.delete()
     # this seems to be more efficiant when we have a lot of data
     for email in emails:
-        delete_email(email).delay()
+        delete_email.delay(email)
 
     # delete tags
     tags = Tag.objects.filter(alias=alias)
     tags.delete()
 
     # okay now mark the alias as deleted
-    alias.deleted = True
     alias.created = datetime.fromtimestamp(0)
     alias.save()
 
     return True
 
-@task(rate_limit=20)
+@task(rate_limit=100, ignore_result=True, store_errors_even_if_ignored=True)
+@transaction.commit_on_success
 def delete_email(email):
     email.delete()
 
