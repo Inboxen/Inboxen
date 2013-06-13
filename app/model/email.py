b1f3bd064d6f5c2b02d8e26b07ab16d0e688e4c1
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-06-13 15:50:01 +0100

    Clean up references to back-end

--- app/model/email.py
+++ app/model/email.py
@@ -2,20 +2,20 @@
 #
 # Copyright 2013 Jessica Tallon, Matt Molyneaux
 # 
-# This file is part of Inboxen back-end.
+# This file is part of Inboxen.
 #
-# Inboxen back-end is free software: you can redistribute it and/or modify
+# Inboxen is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Inboxen back-end is distributed in the hope that it will be useful,
+# Inboxen is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Inboxen back-end.  If not, see <http://www.gnu.org/licenses/>.
+# along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##
 
