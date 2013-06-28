f2f2f5b03cbd565c1a3fcde33ac773d6566f04e5
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-06-28 16:40:32 +0100

    Useful script, will create a manage.py script to replace it at some point

---
+++ tools/requeue.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##
+# Take emails out of the undeliverable queue and dump them in the accepted queue for reprocessing
+##
+
+cd `readlink -e router/run`
+
+TMP_DIR=tmp
+IN_QUEUE=undeliverable/new
+OUT_QUEUE=accepted/new
+
+mkdir -p $TMP_DIR
+
+for file in $IN_QUEUE/*
+do
+    name=`basename $file`
+    grep -v X-Queue-Retry $file > $TMP_DIR/$name
+    $? && rm $file
+done
+
+for file in $TMP_DIR/*
+do
+    mv $file $OUT_QUEUE
+done
+
+echo "Done."
