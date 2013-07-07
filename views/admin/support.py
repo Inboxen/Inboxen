cef484b38e52a58fc58b5d1d33ae844820f755ae
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-07-07 18:21:56 +0100

    Possible fix for #99

--- views/admin/support.py
+++ views/admin/support.py
@@ -22,6 +22,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 
 from inboxen.models import Email, Alias
 
