052976f71d55b1463817e55acfea28a2c36ce674
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-04-15 16:48:56 +0100

    Remove configurable settings from wsgi.py - python-path should be set in HTTPd config files

--- wsgi.py
+++ wsgi.py
@@ -26,16 +26,11 @@ import os
 # os.environ["DJANGO_SETTINGS_MODULE"] = "inboxen.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-# ZE PATHS, ZE PATHS! 
-sys.path.append('ABSOLUTE PATH TO YOUR virtualenv')
-sys.path.append('ABSOLUTE PATH TO erm, where ever the fuck here is')
-
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+# Remember to set python-path in your HTTPd config, see your server
+# documentation for more details
