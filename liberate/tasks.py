138da22341c6808eed061fa031bee7b0daaf859b
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-05-09 02:01:42 +0100

    Don't tell normal users that they're not staff or superusers.
    No need to rub it in their faces.

--- liberate/tasks.py
+++ liberate/tasks.py
@@ -255,8 +255,6 @@ def liberate_user_profile(user_id, email_results, date):
     # user data
     data["id"] = user.id
     data['username'] = user.username
-    data['is_staff'] = user.is_staff
-    data['is_superuser'] = user.is_superuser
     data["is_active"] = user.is_active
     data['last_login'] = user.last_login.isoformat()
     data['join_date'] = user.date_joined.isoformat()
