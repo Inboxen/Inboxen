bfd4b273a4003e36fca09b00ecb1bad55e4a7788
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-06-20 02:09:36 +0100

    Add tests from website and set up travis

---
+++ tests/settings.py
@@ -0,0 +1,24 @@
+import os
+from settings import *
+
+db = os.environ.get('DB')
+
+SECRET_KEY = "This is a test, you don't need secrets"
+
+if db == "sqlite":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        },
+    }
+elif db == "postgres":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'inboxen',
+            'USER': 'postgres',
+        },
+    }
+else:
+    raise NotImplementedError("Please check tests/settings.py for valid DB values")
