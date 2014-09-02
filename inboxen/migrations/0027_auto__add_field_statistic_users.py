708447f46389c2d2566fc41abea128720bad0623
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-09-03 00:22:54 +0100

    Possible fix for migration breakage under Postgres
    I *think* this will fix the problem :)

--- inboxen/migrations/0027_auto__add_field_statistic_users.py
+++ inboxen/migrations/0027_auto__add_field_statistic_users.py
@@ -10,7 +10,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding field 'Statistic.users'
         db.add_column(u'inboxen_statistic', 'users',
-                      self.gf('annoying.fields.JSONField')(default=''),
+                      self.gf('annoying.fields.JSONField')(default={}),
                       keep_default=False)
 
 
