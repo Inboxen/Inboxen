5383bb1701b8fb8fc64b076109d19f98ec20532d
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-09-02 19:34:51 +0100

    Very basic integration test for stats view

---
+++ tests/test_stats.py
@@ -0,0 +1,35 @@
+##
+#    Copyright (C) 2014 Jessica Tallon & Matt Molyneaux
+#   
+#    This file is part of Inboxen.
+#
+#    Inboxen is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Inboxen is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+from django import test
+from django.conf import settings as dj_settings
+from django.contrib.auth import get_user_model
+from django.core import urlresolvers
+
+from inboxen import models
+
+class StatsViewTestCase(test.TestCase):
+    fixtures = ['inboxen_testdata.json']
+    def get_url(self):
+        return urlresolvers.reverse("stats")
+
+    def test_get(self):
+        # just a basic integration test
+        response = self.client.get(self.get_url())
+        self.assertEqual(response.status_code, 200)
