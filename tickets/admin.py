dfc4b82f5254d9d5276ed5371de6ccbf0c2fb73f
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-09-29 15:53:48 +0100

    Remove unused imports from tickets.admin

--- tickets/admin.py
+++ tickets/admin.py
@@ -18,9 +18,6 @@
 ##
 
 from django.contrib import admin
-from django.conf import settings
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 from tickets import models
 
