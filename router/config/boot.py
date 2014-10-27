a8e8435c89160f909a4f1795feb9b095f9d93f66
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-10-27 22:32:36 +0000

    Merge router's settings.py into boot.py

--- router/config/boot.py
+++ router/config/boot.py
@@ -1,14 +1,17 @@
-import os
 import logging
 import logging.config
+import os
+import sys
 
-from config import settings
 from salmon import queue
 from salmon.routing import Router
 from salmon.server import SMTPReceiver, LMTPReceiver
 
-logging.config.fileConfig("config/logging.conf.default")
-logging.config.fileConfig("config/logging.conf")
+sys.path.append('..')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+receiver_config = {'host': 'localhost', 'port': 8823, "type": "smtp"}
+handlers = ['app.server']
 
 try:
     os.mkdir("logs", 0700)
@@ -20,15 +23,20 @@ try:
 except OSError:
     pass
 
-# where to listen for incoming messages
-if settings.receiver_config["type"] == "lmtp":
-    settings.receiver = LMTPReceiver(socket=settings.receiver_config['path'])
-elif settings.receiver_config["type"] == "smtp":
-    settings.receiver = SMTPReceiver(settings.receiver_config['host'],
-                                     settings.receiver_config['port'])
+if os.path.exists("config/logging.conf"):
+    logging.config.fileConfig("config/logging.conf.default")
+else:
+    logging.config.fileConfig("config/logging.conf")
 
-Router.defaults(**settings.router_defaults)
-Router.load(settings.handlers)
+# where to listen for incoming messages
+if receiver_config["type"] == "lmtp":
+    receiver = LMTPReceiver(socket=receiver_config['path'])
+elif receiver_config["type"] == "smtp":
+    receiver = SMTPReceiver(receiver_config['host'],
+                                     receiver_config['port'])
+
+Router.defaults(**router_defaults)
+Router.load(handlers)
 Router.RELOAD=False
 Router.LOG_EXCEPTIONS=True
 Router.UNDELIVERABLE_QUEUE=queue.Queue("run/undeliverable")
