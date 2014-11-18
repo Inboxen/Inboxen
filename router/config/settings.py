45bf0ec3d0221dca60b30e25b7ce01fe61c24652
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-11-18 00:04:24 +0000

    Missing file previous router commit

---
+++ router/config/settings.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+from salmon.server import SMTPReceiver, LMTPReceiver
+
+sys.path.append('..')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from django.conf import settings
+
+# where to listen for incoming messages
+if settings.SALMON_SERVER["type"] == "lmtp":
+    receiver = LMTPReceiver(socket=settings.SALMON_SERVER["path"])
+elif settings.SALMON_SERVER["type"] == "smtp":
+    receiver = SMTPReceiver(settings.SALMON_SERVER['host'],
+                            settings.SALMON_SERVER['port'])
