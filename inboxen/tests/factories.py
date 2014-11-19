ea75f680e9298171b10eb576a2f028c14b380299
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-11-19 00:12:57 +0000

    Start using factory boy
    Doing a lot better with factory boy than with model mommy
    
    [skip ci]

---
+++ inboxen/tests/factories.py
@@ -0,0 +1,66 @@
+##
+#    Copyright (C) 2014 Jessica Tallon & Matt Molyneaux
+#
+#    This file is part of Inboxen.
+#
+#    Inboxen is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Inboxen is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+import datetime
+
+from django.contrib.auth import get_user_model
+
+import factory
+
+from inboxen import models
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+    username = "isdabizda"
+    password = "123456"
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """Call `create_user` rather than `create`
+
+        Happily ignores `django_get_or_create`
+        """
+        manager = cls._get_manager(model_class)
+
+        return manager.create_user(*args, **kwargs)
+
+
+class DomainFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Domain
+
+    domain = factory.Sequence(lambda n: "example%d.com" % n)
+
+
+class InboxFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Inbox
+
+    domain = factory.SubFactory(DomainFactory)
+
+
+class EmailFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Email
+
+    inbox = factory.SubFactory(InboxFactory)
+    received_date = datetime.datetime.now()
