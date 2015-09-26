0fac3a8719c190499f2916672986a133d0428c2b
Jessica Tallon <tsyesika@tsyesika.se>
2015-09-26 14:51:20 +0200

    Support unicode strings in the template tags
    Fix #97

--- website/templatetags/inboxen_account.py
+++ website/templatetags/inboxen_account.py
@@ -16,6 +16,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
 ##
+from __future__ import unicode_literals
 
 from django import template
 from django.utils import safestring
