446a1c3412815412a3db5da25fc399daa40a28aa
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-05-15 12:20:04 +0100

    Collect stats on "active"ness
    E.g. how many inboxes actually have emails
    
    touch #148

--- inboxen/views/stats.py
+++ inboxen/views/stats.py
@@ -42,13 +42,31 @@ def stats_recent(request):
     objects = reversed(models.Statistic.objects.order_by("-date")[:90])
     dates = []
     users = []
+    active_users = []
     inboxes = []
+    active_inboxes = []
     emails = []
+    read_emails = []
 
     for stat in objects:
         dates.append(stat.date)
-        users.append(stat.users.get("count"))
-        inboxes.append(stat.inboxes.get("inbox_count__sum"))
-        emails.append(stat.emails.get("email_count__sum"))
 
-    return JsonResponse({"dates": dates, "users": users, "inboxes": inboxes, "emails": emails, "now": timezone.now()})
+        users.append(stat.users.get("count"))
+        active_users.append(stat.users.get("with_inboxes"))
+
+        inboxes.append(stat.inboxes.get("inbox_count__sum"))
+        active_inboxes.append(stat.inboxes.get("with_emails"))
+
+        emails.append(stat.emails.get("email_count__sum"))
+        read_emails.append(stat.emails.get("emails_read"))
+
+    return JsonResponse({
+        "dates": dates,
+        "users": users,
+        "active_users": active_users,
+        "inboxes": inboxes,
+        "active_inboxes": active_inboxes,
+        "emails": emails,
+        "read_emails": read_emails,
+        "now": timezone.now()
+    })
