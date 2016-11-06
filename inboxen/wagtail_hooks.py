8db76aa58d46985d94e47826919203dc6e11dca8
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-11-06 12:10:05 +0000

    Prevent admins from creating Request objects
    touch #162

--- inboxen/wagtail_hooks.py
+++ inboxen/wagtail_hooks.py
@@ -1,6 +1,12 @@
+from wagtail.contrib.modeladmin.helpers import PermissionHelper
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
 from inboxen import models
+
+
+class RequestPermissionHelper(PermissionHelper):
+    def user_can_create(self, user):
+        return False
 
 
 class DomainAdmin(ModelAdmin):
@@ -13,6 +19,7 @@ class RequestAdmin(ModelAdmin):
     model = models.Request
     menu_icon = 'arrow-up-big'
     list_display = ("requester", "date", "amount", "succeeded")
+    permission_helper_class = RequestPermissionHelper
 
 
 modeladmin_register(DomainAdmin)
