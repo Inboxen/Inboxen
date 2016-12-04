3dd35deeea39641f351601a058af9ec5705c7000
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-12-04 11:29:03 +0000

    `inboxen.admin` no longer exists
    touch #162

--- inboxen/urls.py
+++ inboxen/urls.py
@@ -27,7 +27,7 @@ from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
-from inboxen import admin, views
+from inboxen import views
 
 
 urls.handler400 = views.error.bad_request
