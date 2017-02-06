5796a44fe0210b4b7d9d2a50c3d5715521e32995
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2017-02-06 13:24:42 +0000

    Automatically select current admin when creating blog post
    touch #162

--- blog/wagtail_hooks.py
+++ blog/wagtail_hooks.py
@@ -1,6 +1,23 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail.contrib.modeladmin.views import CreateView, EditView
 
 from blog.models import BlogPost
+
+
+class InstanceAuthorMixin(object):
+    def get_instance(self):
+        instance = super(InstanceAuthorMixin, self).get_instance()
+        if instance.author_id is None:
+            instance.author = self.request.user
+        return instance
+
+
+class BlogCreateView(InstanceAuthorMixin, CreateView):
+    pass
+
+
+class BlogEditView(InstanceAuthorMixin, EditView):
+    pass
 
 
 class BlogAdmin(ModelAdmin):
@@ -12,6 +29,9 @@ class BlogAdmin(ModelAdmin):
         return not obj.draft
     published.short_description = "Published?"
     published.boolean = True
+
+    create_view_class = BlogCreateView
+    edit_view_class = BlogEditView
 
 
 modeladmin_register(BlogAdmin)
