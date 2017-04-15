39fc073fd333af6b1d686da8a73dba23871ec5d4
Jessica Tallon <tsyesika@tsyesika.se>
2017-04-15 08:12:10 +0200

    Fix #194 - Add missing copyright headers

--- cms/views.py
+++ cms/views.py
@@ -1,5 +1,23 @@
-from django.http import Http404
+##
+#    Copyright (C) 2013, 2014, 2015, 2016, 2017 Jessica Tallon & Matt Molyneaux
+#
+#    This file is part of Inboxen.
+#
+#    Inboxen is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Inboxen is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
+##
 
+from django.http import Http404
 
 def user_editing_disabled_view(request):
     raise Http404("User editing disabled")
