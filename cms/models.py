03ee2bff908222343bd8b3105509b975525d3954
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-01-10 21:28:35 +0000

    Turn headings into links
    Useful for linking to specific sections

--- cms/models.py
+++ cms/models.py
@@ -35,7 +35,7 @@ from inboxen import validators
 
 HELP_PAGE_TAGS = DEFAULT_ALLOW_TAGS + ["h%s" % i for i in xrange(1, 6)]
 HELP_PAGE_ATTRS = DEFAULT_SAFE_ATTRS + ["id"]
-HELP_PAGE_EXTENSIONS = DEFAULT_MARKDOWN_EXTENSIONS + [TocExtension()]
+HELP_PAGE_EXTENSIONS = DEFAULT_MARKDOWN_EXTENSIONS + [TocExtension(anchorlink=True)]
 
 
 class HelpQuerySet(TreeQuerySet):
