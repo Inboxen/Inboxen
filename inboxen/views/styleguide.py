81b2cf35afa387176e836c7840147e357bff224e
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-02-17 23:58:10 +0000

    Generate mock inboxes/emails
    …and use template includes for those objects

--- inboxen/views/styleguide.py
+++ inboxen/views/styleguide.py
@@ -17,11 +17,18 @@
 #    along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
+from datetime import timedelta
+
+from bitfield import BitHandler
 from django import forms
-from django.views.decorators.http import require_GET
-from django.template.response import TemplateResponse
-from django.contrib.messages.utils import get_level_tags
 from django.contrib.messages.constants import DEFAULT_LEVELS
+from django.contrib.messages.utils import get_level_tags
+from django.template.response import TemplateResponse
+from django.utils import timezone
+from django.views.decorators.http import require_GET
+import mock
+
+from inboxen.forms.inbox import InboxEditForm
 
 
 class Form(forms.Form):
@@ -33,7 +40,58 @@ class Form(forms.Form):
 
 @require_GET
 def styleguide(request):
+    now = timezone.now() + timedelta(-1)
+    domain = mock.Mock(domain="example.com")
+
+    # create a bunch of mocked inboxes
+    inboxes = [
+        mock.Mock(
+            inbox="qwerty",
+            domain=domain,
+            flags=BitHandler(0, ["new"]),
+            last_activity=now,
+            form=InboxEditForm(request),
+        ),
+        mock.Mock(
+            inbox="qwerty",
+            domain=domain,
+            flags=BitHandler(1, ["disabled"]),
+            last_activity=now,
+            form=False,
+        ),
+        mock.Mock(inbox="qwerty",
+            domain=domain,
+            flags=BitHandler(1 | 2, ["new", "pinned"]),
+            last_activity=now,
+            form=False,
+        ),
+    ]
+
+    # emails
+    emails = [
+         mock.Mock(
+            inbox=inboxes[0],
+            flags=BitHandler(1, ["important"]),
+            received_date=now,
+        ),
+        mock.Mock(
+            inbox=inboxes[0],
+            flags=BitHandler(0, ["important"]),
+            received_date=now,
+        ),
+    ]
+
+
+    # attachments
+    attachments = [
+        mock.Mock(id=0, filename=("a" * 100), content_type="blah/blah", get_children=[]),
+        mock.Mock(id=0, filename="a", content_type=None, get_children=[]),
+    ]
+
     context = {
+        "inboxes": inboxes,
+        "emails": emails,
+        "attachments": attachments,
         "form": Form(),
         "message_types": [(k, get_level_tags()[v]) for k, v in DEFAULT_LEVELS.items() if k != 'DEBUG'],
     }
