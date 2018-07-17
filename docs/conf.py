29ffb697863b95f586d80d1b90a6284c864212c3
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-07-17 21:17:52 +0100

    Update year

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Inboxen'
-copyright = u'2015, Jessica Tallon & Matt Molyneaux'
+copyright = u'2015, 2018 Jessica Tallon & Matt Molyneaux'
 author = u'Jessica Tallon & Matt Molyneaux'
 
 # The version info for the project you're documenting, acts as replacement for
