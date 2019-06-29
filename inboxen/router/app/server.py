b7bb6004e61888ba5998d6d1723e58d73eb17871
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2019-06-29 09:41:11 +0100

    Fix license headers
    IN MASTER

--- inboxen/router/app/server.py
+++ inboxen/router/app/server.py
@@ -1,22 +1,20 @@
 ##
+#    Copyright 2013, 2015 Jessica Tallon, Matt Molyneaux
 #
-# Copyright 2013, 2015 Jessica Tallon, Matt Molyneaux
+#    This file is part of Inboxen.
 #
-# This file is part of Inboxen.
+#    Inboxen is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# Inboxen is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#    Inboxen is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# Inboxen is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
-#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
 import logging
