c093aa269fbd8541dbbe40edc48f1d6d01c7c3f7
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2019-07-19 15:55:15 +0100

    Salmon server should be started via boot not settings
    This corrects a minor error that could lead to Salmon starting a server
    when it didn't mean to. Currently this isn't actually a problem because
    the only command other than "start" that could load settings is broken:
    https://github.com/moggers87/salmon/issues/102

--- inboxen/router/config/settings.py
+++ inboxen/router/config/settings.py
@@ -1,16 +1,7 @@
 import os
 
-from django.conf import settings  # noqa
-from salmon.server import LMTPReceiver, SMTPReceiver
-import django  # noqa
+import django
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'inboxen.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inboxen.settings')
 
 django.setup()
-
-# where to listen for incoming messages
-if settings.SALMON_SERVER["type"] == "lmtp":
-    receiver = LMTPReceiver(socket=settings.SALMON_SERVER["path"])
-elif settings.SALMON_SERVER["type"] == "smtp":
-    receiver = SMTPReceiver(settings.SALMON_SERVER['host'],
-                            settings.SALMON_SERVER['port'])
