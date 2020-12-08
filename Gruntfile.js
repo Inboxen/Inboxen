c7f4b6001d4188898a05c56101807096fc8563a0
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2020-12-08 14:44:56 +0000

    JSHint shouldn't check everything
    IN MASTER

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function(grunt) {
         },
         jshint: {
             options: {jshintrc: true},
-            all: ["."]
+            all: ["<%= dirs.js %>"]
         }
     });
 
