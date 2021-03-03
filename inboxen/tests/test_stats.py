931249b37e53b54366386083b1b6257813849c2a
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2021-03-03 12:25:24 +0000

    Fix minor linting issue in tests
    AM MAIN

--- inboxen/tests/test_stats.py
+++ inboxen/tests/test_stats.py
@@ -84,8 +84,10 @@ class StatsViewTestCase(InboxenTestCase):
     def test_csp(self):
         # test a normal view
         response = self.client.get(reverse("index"))
-        self.assertIn("style-src 'self'", [i.strip() for i in response["content-security-policy"].split(";")])
+        self.assertIn("style-src 'self'",
+                      [i.strip() for i in response["content-security-policy"].split(";")])
 
         # stats view
         response = self.client.get(reverse("stats"))
-        self.assertIn("style-src 'self' 'unsafe-inline'", [i.strip() for i in response["content-security-policy"].split(";")])
+        self.assertIn("style-src 'self' 'unsafe-inline'",
+                      [i.strip() for i in response["content-security-policy"].split(";")])
